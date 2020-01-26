d2c89d6434ee989f38e6e10c285412fc2a6ad4af
Botty <botty@uvhotspot.com>
2020-01-26 15:47:50 +0000

    adverarial explanations code

--- demo/amy_demo/adv_model_generator.py
+++ demo/amy_demo/adv_model_generator.py
@@ -1,30 +1,34 @@
 import tensorflow as tf
 
-if __name__=='__main__':
+if __name__ == '__main__':
     tf.enable_eager_execution()
 
+from evaluate import *
 
 
-def get_adversarial_model(X_train, Y_train, X_test, Y_test, e_alpha=0.25, feature_set=None, EPOCHS=1000, R_EPOCHS=100, batch_size=30000,
-                              seed=49, num_layers=3):
+def adversarial_explanation_wrapper(X_train, Y_train, model_full, z_idx, e_alpha=0.25, verbose=0):
+    train_dataset = get_dataset(X_train, Y_train, batch_size=1000)
+    model_explain = clone_model(model_full)
+    optimizer = tf.keras.optimizers.Adam(lr=0.01)
+    for t in range(50):
+        train_err, train_loss = epoch_explanation(train_dataset, model_explain, attack=no_attack,
+                                                  sensitive_feature_id=z_idx, e_alpha=e_alpha,
+                                                  optimizer=optimizer)
 
-    models_p = []
-    adv_lis = []
-    val_names = ("train_err", "test_err", "adv_err", "adv_err_fgsm", "e_loss", "e_loss_train")
-    print(*("{}".format(i) for i in val_names), sep="\t")
-    if feature_set is None:
-        feature_set = range(n_features)
-    for i in feature_set:
-        print(i)
-        z_idx = i
-        model_explain = clone_model(model_full)
-        optimizer = tf.keras.optimizers.Adam(lr=0.01)
-        for t in range(50):
-            train_err, train_loss = epoch_explanation(adult_train, model_explain, attack,
-                                                      sensitive_feature_id=z_idx, e_alpha=e_alpha,
-                                                      epsilon=0.25, alpha=0.08, num_iter=30, optimizer=optimizer)
-        models_p.append(model_explain)
-        adv_err, adv_err_f, e_loss, e_loss_train, test_err = epoch_eval(adult_train, adult_test, model_explain, z_idx)
-        r = (train_err, test_err, adv_err, adv_err_f, e_loss, e_loss_train)
-        adv_lis.append(r)
-        print(*("{:.6f}".format(i) for i in r), sep="\t")
+    return model_explain
+
+
+def evaluate_adversarial_explanation(X_train, Y_train, X_test, Y_test, model_explain, z_idx):
+    raise NotImplementedError
+    train_dataset = get_dataset(X_train, Y_train, batch_size=1000)
+    test_dataset = get_dataset(X_test, Y_test, batch_size=1000)
+    adv_err, adv_err_f, e_loss, e_loss_train, test_err = epoch_eval(train_dataset, test_dataset, model_explain, z_idx)
+    r = (train_err, test_err, adv_err, adv_err_f, e_loss, e_loss_train)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- demo/amy_demo/financial.py
+++ demo/amy_demo/financial.py
@@ -7,30 +7,30 @@ from demo.data_loaders.uci_datasets import *
 from dginn.core import *
 from aggregator_utils import compute_dg_per_datapoint, extract_dgs_by_ids
 from demo.amy_demo.model_build import *
-
+from .adv_model_generator import adversarial_explanation_wrapper
 
 # Load data
 sensitive_feature_name = None # TODO: experiment with sensitive features
 file_path = "/Users/AdminDK/Desktop/Datasets/adult/" # TODO: load from config file
 Xtr, Xts, ytr, yts, _, _ = get_adult(None, file_path=file_path)
-X_train, _, y_train, _ = prep_data(Xtr, Xts, ytr, yts, verbose=1)
+X_train, _, Y_train, _ = prep_data(Xtr, Xts, ytr, yts, verbose=1)
 
 # Build basic model with a set of Dense layers
 model = build_model()
 
 # Fit model to the data
-fit_model(model, X_train, y_train, verbose=1)
+fit_model(model, X_train, Y_train, verbose=1)
 
 # Generate adversarially-trained model
-
+z_idx = adult_sensitive_features_dict["age"]
+model_ae = adversarial_explanation_wrapper(X_train, Y_train, model, z_idx=z_idx, e_alpha=0.25)
 
 # Extract feature importances using different methods
 feature_importance_methods = []
 
-for feature_importance_method in feature_importance_methods:
 
-    # Compute and compare feature importances for both the original and adversarially-trained model
-    feature_importance_orig = ...
-    feature_importance_adv = ...
+def main():
+    e
 
-    # TODO: how to compare? draw figures?
+if __name__ == '__main__':
+    main
