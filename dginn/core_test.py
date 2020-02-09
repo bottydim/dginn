52904db911a453d44516505fd79af17e89548224
Dmitry Kazhdan <dmitry@kazhdan.net>
2020-02-09 21:23:11 +0000

    add testing code

---
+++ dginn/core_test.py
@@ -0,0 +1,58 @@
+import numpy as np
+import tensorflow as tf
+from dginn.dginn.core import *
+
+
+def build_model(input_shape=(14,), num_class=2):
+
+    num_layers = 2
+    num_hidden = 100
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Input(shape=input_shape))
+
+    for i in range(num_layers):
+        model.add(tf.keras.layers.Dense(num_hidden, activation=tf.nn.relu, kernel_regularizer=tf.keras.regularizers.l2(0.03)))
+
+    model.add(tf.keras.layers.Dense(num_class, activation=tf.nn.softmax))
+
+    optimizer = tf.keras.optimizers.RMSprop(0.01, decay=0.005)
+
+    loss = tf.keras.losses.categorical_crossentropy
+
+    model.compile(loss=loss,
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
+
+    return model
+
+
+
+
+def dep_graph_test():
+
+    n_samples = 50
+    n_features = 10
+
+    x_train = np.random.uniform(0.0, 10.0, (n_samples, n_features))
+    y_train = np.random.randint(low=0, high=2, size=(n_samples))
+    y_train = tf.keras.utils.to_categorical(y_train)
+
+    model = build_model((n_features,), 2)
+
+    model.fit(x_train, y_train, epochs=200, verbose=True)
+
+    loss = lambda x: x # Use the identify for neuron preprocessing
+    agg_data_points = False
+    agg_neurons = False
+
+    grad_computer = Gradients_Computer(model, loss= loss,
+                                       agg_data_points=agg_data_points,
+                                       agg_neurons=agg_neurons)
+
+    dep_graph = DepGraph(grad_computer)
+
+
+dep_graph_test()
+
+
